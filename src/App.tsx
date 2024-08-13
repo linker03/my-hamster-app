aad85f8db8cd5928d0ed92c2e791bcaf405472ed
Ilya Pavilonets <ilya.pavilonets@volga-dnepr.com>
2024-08-13 20:38:50 +0600

    add progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import 'swiper/css/navigation';
 
 import { Carousel } from './components/Carousel';
 import { UpgradePanel } from './components/UpgradePanel';
+import { ProgressBar } from './components/ProgressBar';
 
 function App() {
   return (
     <>
       <Carousel />
-      <UpgradePanel />
+      <div className="content-container">
+        <ProgressBar totalLevels={3} currentLevel={1} />
+        <UpgradePanel />
+      </div>
+
       <img className="footer" src="/footer.png" alt="" />
     </>
   );
---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import styles from './ProgressBar.module.css';
+
+interface ProgressBarProps {
+  totalLevels?: number;
+  currentLevel?: number;
+}
+
+export const ProgressBar = ({
+  totalLevels = 1,
+  currentLevel = 0,
+}: ProgressBarProps) => {
+  const targetProgress = Math.floor((100 / totalLevels) * (currentLevel + 1));
+  const currentProgress = Math.floor((100 / totalLevels) * currentLevel);
+  return (
+    <div className={styles.root}>
+      <div
+        className={styles.targetProgress}
+        style={{ width: `${targetProgress}%` }}
+      />
+      <div
+        className={styles.progress}
+        style={{ width: `${currentProgress}%` }}
+      />
+    </div>
+  );
+};
